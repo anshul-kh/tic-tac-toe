7939f2b657566512cb2065e866e7df18e051a27a
Yuichi <yuichi@pop-os.localdomain>
2023-08-08 20:53:51 +0530

    Added Responsiveness

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -3,13 +3,11 @@ import React from "react";
 const  Square = (props) => {
     return(
         <div className="square" style={{ border:"1px solid", 
-        width: "100%" ,
-        height:"100px" ,
         display:"flex" ,
         justifyContent : "center", 
         alignItems: "center",
         fontSize: "50px",
-        backgroundColor: props.value === "X" ? "#103f9dea":"#565656" 
+        backgroundColor: props.value === "X" ? "#6fd6cf3c" : "#1D1E27"
      }}
         onClick = {props.onclick}>
             <h5>{props.value}</h5>
